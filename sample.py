3b456e1304f71a779b4ff4319aa416f433a90862
ARUL R <arulskumar82@gmail.com>
2026-07-29 15:31:42 +0530

    Commit message you gonna do

--- sample.py
+++ sample.py
@@ -1,3 +1,14 @@
 a="arul"
 b="kumar"
 print(a+""+b)
+class stu:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def display(self):
+        print(self.name)
+        print(self.age)
+name=input("enter the name:")
+age=int(input("enter the age:"))
+obj=stu(name,age)
+obj.display()
